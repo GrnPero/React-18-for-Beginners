cdccedd0a461d4b7792d3f3074fb9fc8debed352
Rodrigo Alvarez <ralvarez@lds.local>
2023-04-17 12:00:46 -0700

    Made the neccessary files and code to get this project started

---
+++ src/components/Like.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// Have to add props to the function
+// const Like = (props) => {
+// And add react icons to the project
+
+const Like = () => {
+  return <div>Like</div>;
+};
+
+export default Like;
