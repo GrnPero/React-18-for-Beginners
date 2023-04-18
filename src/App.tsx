2db106f326f2331cb68a32868efc762e99081539
Rodrigo Alvarez <ralvarez@lds.local>
2023-04-18 11:45:37 -0700

    My solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import Like from "./components/Like";
+import { useState } from "react";
 
 function App() {
+  const [showLike, setShowLike] = useState(false);
+
   return (
     <div>
-      <Like onClick={() => console.log("clicked")} />
+      <Like
+        showLike={showLike}
+        onClick={() => {
+          if (showLike) setShowLike(false);
+          else setShowLike(true);
+          console.log("Like clicked");
+        }}
+      />
     </div>
   );
 }
--- src/components/Like.tsx
+++ src/components/Like.tsx
@@ -1,11 +1,20 @@
-import React from "react";
+import { AiOutlineHeart } from "react-icons/ai";
+import { FcLike } from "react-icons/fc";
 
 // Have to add props to the function
 // const Like = (props) => {
 // And add react icons to the project
+interface Props {
+  showLike: boolean;
+  onClick: () => void;
+}
 
-const Like = () => {
-  return <div>Like</div>;
+const Like = ({ showLike, onClick }: Props) => {
+  return (
+    <div onClick={onClick}>
+      {showLike ? <FcLike size={40} /> : <AiOutlineHeart size={40} />}
+    </div>
+  );
 };
 
 export default Like;
