ea7a726160ded3f3bd74b03938f64f4ad8b7e6f4
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2023-10-20 20:16:44 -0700

    Learned how to updat eobjects using states

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import Message from "./components/Message";
+import { useState } from "react";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Coca Cola",
+    price: 2.5,
+  });
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: 3.5 });
+  };
+
   return (
     <div>
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
