5ed43efa61ba561988b5357e4cf9efb13407c356
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2023-10-20 22:03:56 -0700

    Learned how to update arrays

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import { useState } from "react";
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "John",
-    address: {
-      city: "New York",
-      zipCode: 10001,
-    },
-  });
+  const [tags, setTags] = useState(["happy", "cheerful"]);
 
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 94112 },
-    });
+    // Add
+    setTags([...tags, "exciting"]);
+
+    // Remove
+    setTags(tags.filter((tag) => tag !== "happy"));
+
+    // Update
+    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
   };
 
   return (
     <div>
-      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick}></button>
     </div>
   );
 }
