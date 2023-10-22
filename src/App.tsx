04ff5b25b2c5dc50d655c79d90cd4d0e5c9a1a08
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2023-10-22 16:09:18 -0700

    Building out a form in react with bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import React from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxLength={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero aperiam ea
-        quo deleniti recusandae aliquid, laboriosam earum in maiores voluptates
-        voluptatibus vitae nobis soluta neque dolore id quaerat delectus ratione
-        cupiditate, pariatur ipsam ut culpa doloribus eos. Animi deserunt, ea
-        dicta error dolorem quaerat aperiam provident repudiandae excepturi,
-        quas nostrum harum optio voluptatem adipisci a quia alias iure!
-        Provident modi odio molestias, eius doloremque assumenda consequatur ab,
-        totam fuga impedit voluptates! Laboriosam, dolores. Magni assumenda,
-        obcaecati fuga laboriosam voluptas natus ipsam provident! Inventore
-        repudiandae voluptatum nam. Cumque numquam officia magni! Adipisci
-        laboriosam deleniti quidem laborum soluta assumenda, iure provident nam.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
