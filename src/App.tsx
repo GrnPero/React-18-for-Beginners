d894b2b269d358640232509d6e6dc0f74d247018
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2023-10-22 14:07:50 -0700

    Exercise 1 Solved!

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
+import React from "react";
 import { useState } from "react";
-import NavBar from "./components/NavBar";
-import Cart from "./components/Cart";
 
 function App() {
-  const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const handleClick = () => {
+    setGame({
+      ...game,
+      player: {
+        ...game.player,
+        name: "Jane",
+      },
+    });
+  };
 
   return (
     <div>
-      <NavBar cartItemsCount={cartItems.length}></NavBar>
-      <Cart cartItems={cartItems} onClear={() => setCartItems([])}></Cart>
+      <h1>{game.player.name}</h1>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
