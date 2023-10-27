64ece87f74771a6a2e719013a6271ca7d1467f2e
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2023-10-27 13:35:52 -0700

    Learned that effect hooks occurs after render

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,21 @@
-import React, { useState } from "react";
-import Form from "./components/Form";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
+import React, { useEffect, useRef } from "react";
 
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 20, category: "Food" },
-    { id: 3, description: "ccc", amount: 30, category: "Transportation" },
-  ]);
+  // After Renders
+  useEffect(() => {
+    // Side effect
+    if (ref.current) ref.current.focus();
+  });
 
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((expense) => expense.category === selectedCategory)
-    : expenses;
+  useEffect(() => {
+    document.title = "Hello World";
+  });
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => {
-          setExpenses(expenses.filter((expense) => expense.id !== id));
-        }}
-      />
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
 }
