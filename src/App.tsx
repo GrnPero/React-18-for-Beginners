d09349482e83e1640995fee7e6091a18c68c14cc
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2023-10-27 13:52:01 -0700

    Learned about clean up code for dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,21 @@ import React, { useEffect, useRef, useState } from "react";
 import ProductList from "./components/ProductList";
 
 function App() {
-  const ref = useRef<HTMLInputElement>(null);
+  const connect = () => {
+    console.log("Connecting");
+  };
 
-  const [category, setCategory] = useState("");
+  const disconnect = () => {
+    console.log("Disconnecting");
+  };
 
-  return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
-  );
+  useEffect(() => {
+    connect();
+
+    return () => disconnect();
+  });
+
+  return <div></div>;
 }
 
 export default App;
