af6ea9a4fcc91b2987c4d54ec81c37e3e334bc2b
Rodrigo Alvarez <r.alvarezcobarruvias@gmail.com>
2023-10-27 14:15:40 -0700

    Another method in making a request he likes the then,catch

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 interface User {
   id: number;
@@ -11,12 +11,24 @@ function App() {
   const [error, setError] = useState("");
 
   useEffect(() => {
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/xusers")
-      .then((res) => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get<User[]>(
+          "https://jsonplaceholder.typicode.com/xusers"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError((err as AxiosError).message);
+      }
+    };
+
+    fetchUsers();
+    // get -> promise -> res / err
+
+    /* .then((res) => {
         setUsers(res.data);
       })
-      .catch((err) => setError(err.message));
+      .catch((err) => setError(err.message));*/
   }, []);
 
   return (
